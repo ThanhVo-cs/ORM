a5dc05da938ab3cc5a6d68d7874a2633c28c9260
ThanhVo <31005301+ThanhVoo@users.noreply.github.com>
2023-05-29 12:55:07 +0700

    create Entity class use Anotation @Entity

--- JPA/src/main/java/orm/jpa/Student.java
+++ JPA/src/main/java/orm/jpa/Student.java
@@ -2,9 +2,19 @@ package orm.jpa;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "student")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 150)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 200)
     private String lastName;
 }
