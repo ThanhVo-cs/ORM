164059e9329b973c5641bd95f980a7d65f967fc3
ThanhVo <31005301+ThanhVoo@users.noreply.github.com>
2023-05-29 17:47:29 +0700

    create file persistence.xml and create a Object use EntityManagerFactory

---
+++ JPA/src/main/java/orm/jpa/model/Student.java
@@ -0,0 +1,51 @@
+package orm.jpa.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 150)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 200)
+    private String lastName;
+
+    public Student() {
+    }
+
+    public Student(Long id, String fistName, String lastName) {
+        this.id = id;
+        this.firstName = fistName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
